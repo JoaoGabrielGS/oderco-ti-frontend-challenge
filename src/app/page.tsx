31dc22db8a0d7e7864ac928a77147357726bfca9
João Gabriel <bielgolin@gmail.com>
2024-10-10 17:40:56 -0300

    (chore): add react-icons package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { FiShoppingCart } from "react-icons/fi";
+
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-2xl">Projeto Dashboard</h1>
       <Button>Test</Button>
+      <h3> Lets go for a <FiShoppingCart size={100} color={'#123456'} />? </h3>
     </main>
   );
 }

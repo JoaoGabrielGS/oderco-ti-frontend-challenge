4ae95422bc807a423ec49d7abe51b9dde0b3b092
João Gabriel <bielgolin@gmail.com>
2024-10-20 22:56:17 -0300

    fix(build): regras eslint

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+export default [
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    }
+  }
+];

f794ff9d1f59452a6efe9da26c7bfe3cc4164565
João Gabriel <bielgolin@gmail.com>
2024-10-20 23:00:57 -0300

    fix(eslint): fix lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,13 @@
-export default [
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-empty-object-type': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-    }
-  }
-];
+import { Linter } from 'eslint';
+
+/** @type {Linter.Config} */
+const config = {
+  extends: ['next/core-web-vitals', 'next/typescript'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-object-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+  },
+};
+
+export default config;
